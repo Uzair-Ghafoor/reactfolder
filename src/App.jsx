6df1ff218a6b9fc82850499c8b253e97b2ee6e95
Uzair-Ghafoor <uzairgbgrt@gmail.com>
2024-10-01 12:11:02 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import React, { useState } from 'react';
 import data from './data.json';
 import Component from './Component';
 const App = () => {
+  const [name, setName] = useState('hello');
+
   return (
-    <div className=' flex flex-wrap gap-4'>
-      {data.map((link) => {
-        return <Component {...link} key={link.id} />;
-      })}
-      <div>hello</div>
-    </div>
+    <>
+      <div>{name}</div>
+      <button onClick={() => setName('world')}></button>
+    </>
+    // <div className=' flex flex-wrap gap-4'>
+    //   {data.map((link) => {
+    //     return <Component {...link} key={link.id} />;
+    //   })}
+    //   <div>hello</div>
+    // </div>
   );
 };
 
