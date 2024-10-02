bd1f879818649cca685fe5d0a1530719c008a3f0
Uzair-Ghafoor <uzairgbgrt@gmail.com>
2024-10-02 12:20:28 +0500

    axions

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
-import data from './data.json';
+import React, { useEffect, useState } from 'react';
 import Component from './Component';
-const App = () => {
-  const [name, setName] = useState('hello');
+import axios from 'axios';
+const url = 'https://www.course-api.com/react-tours-project';
 
+const App = () => {
+  const [data1, setData] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = await axios.get(url);
+      setData(data.data);
+    };
+    getData();
+  }, []);
+  console.log(data1);
   return (
-    <>
-      <div>{name}</div>
-      <button onClick={() => setName('world')}></button>
-    </>
-    // <div className=' flex flex-wrap gap-4'>
-    //   {data.map((link) => {
-    //     return <Component {...link} key={link.id} />;
-    //   })}
-    //   <div>hello</div>
-    // </div>
+    <div className=' bg-gray-200'>
+      <div className=' grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-7  max-w-[75vw] mx-auto pt-14'>
+        {data1.map((list) => {
+          return <Component {...list} key={list.id} />;
+        })}
+      </div>
+    </div>
   );
 };
 
--- src/Component.jsx
+++ src/Component.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Component = (props) => {
+const Component = ({ image, info, name, price }) => {
   return (
-    <div
-      className={` p-5 flex flex-col rounded-lg gap-y-4 ${
-        props.isTrue ? 'bg-red-500' : 'bg-gray-200'
-      }`}
-    >
-      <h2>{props.name}</h2>
-      <p>{props.email}</p>
+    <div className=' max-w-[20vw] bg-white relative rounded-lg'>
+      <img
+        src={image}
+        className=' w-full h-[30vh] rounded-t-lg object-cover'
+        alt=''
+      />
+      <div className='p-5 flex flex-col gap-y-3'>
+        <h1 className=' px-2 text-center font-semibold text-xl'>{name}</h1>
+        <p>{info}</p>
+      </div>
+      <p className=' absolute top-0 right-0 rounded-tr-lg bg-green-600 text-white text-xl p-2 '>
+        ${price}
+      </p>
     </div>
   );
 };
 
 export default Component;
+// import React from 'react';
+
+// const Component = ({name,email}) => {
+//   const { name, email } = props;
+//   return (
+//     <div
+//       className={` p-5 flex flex-col rounded-lg gap-y-4 ${
+//         props.isTrue ? 'bg-red-500' : 'bg-gray-200'
+//       }`}
+//     >
+//       <h2>{props.name}</h2>
+//       <p>{props.email}</p>
+//     </div>
+//   );
+// };
+
+// export default Component;
